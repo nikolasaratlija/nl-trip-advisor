3d604df777e0f5497628515a2a613584fcf65df2
Nikola Saratlija <nikola@brandsync.nl>
2019-04-05 23:45:05 +0200

    forgot to add js file responsible for instantiating the Google Maps map

--- public/initMap.js
+++ public/initMap.js
@@ -3,7 +3,7 @@ function initMap() {
         lat: 51.9054439,
         lng: 4.4666374,
         defaultZoom: 12,
-        KmlLayerLink: "https://www.dropbox.com/s/j69qh1g9wbkutg5/rotterdam.kml?dl=1"
+        KmlLayerLink: "https://www.dropbox.com/s/j69qh1g9wbkutg5/rotterdam.kml?dl=1",
     };
 
     const styles = [

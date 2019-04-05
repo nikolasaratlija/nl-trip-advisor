0cdbbbf5496da5077ddad38c5d9cc6e2cd4e5ee5
Nikola Saratlija <nikola@brandsync.nl>
2019-04-06 00:09:39 +0200

    fixed css by renaming a css class appropriately

--- src/components/TripAdvisor/TripAdvisor.js
+++ src/components/TripAdvisor/TripAdvisor.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import "./styles/_Guide.scss"
+import "./styles/TripAdvisor.scss"
 
 import Info from "./Info";
 
@@ -13,13 +13,13 @@ class TripAdvisor extends Component {
         return (
             <div id={"TripAdvisor"} className={"TripAdvisor"}>
                 <div className="control left" id="control-left">
-                    <i className="fas fa-caret-left fa-2x white"></i>
+                    <i className="fas fa-caret-left fa-2x white"/>
                 </div>
 
                 <Info/>
 
                 <div className="control right" id="control-right">
-                    <i className="fas fa-caret-right fa-2x white"></i>
+                    <i className="fas fa-caret-right fa-2x white"/>
                 </div>
             </div>
         );

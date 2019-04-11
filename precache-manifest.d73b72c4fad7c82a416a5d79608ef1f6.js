65967708e6b8591a3ed3405d214942aca053c234
Nikola Saratlija <nikola@brandsync.nl>
2019-04-11 21:24:05 +0200

    Updates

--- precache-manifest.d73b72c4fad7c82a416a5d79608ef1f6.js
+++ precache-manifest.d73b72c4fad7c82a416a5d79608ef1f6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/nl-trip-advisor-react/static/js/runtime~main.09a7ef84.js"
   },
   {
-    "revision": "ccc0e405ed4070e7f682",
-    "url": "/nl-trip-advisor-react/static/js/main.31bba37a.chunk.js"
+    "revision": "a181bbfd939b8a198f10",
+    "url": "/nl-trip-advisor-react/static/js/main.fd433a07.chunk.js"
   },
   {
     "revision": "f51d143bbccedb5ea87d",
     "url": "/nl-trip-advisor-react/static/js/2.da21de22.chunk.js"
   },
   {
-    "revision": "ccc0e405ed4070e7f682",
-    "url": "/nl-trip-advisor-react/static/css/main.d22db49c.chunk.css"
+    "revision": "a181bbfd939b8a198f10",
+    "url": "/nl-trip-advisor-react/static/css/main.f2572cbf.chunk.css"
   },
   {
-    "revision": "205dba584fec88f01c1d2a5cedd23a55",
+    "revision": "ffa4a23f4edbadba9961e2e3b4987cbc",
     "url": "/nl-trip-advisor-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nl-trip-advisor-react/precache-manifest.d80f9d34b059a82bade1a0c0712106b9.js"
+  "/nl-trip-advisor-react/precache-manifest.d73b72c4fad7c82a416a5d79608ef1f6.js"
 );
 
 workbox.clientsClaim();

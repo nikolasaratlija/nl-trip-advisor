b95712814d204bb576494c10976b36d46abfa96a
Nikola Saratlija <nikola@brandsync.nl>
2019-04-11 18:54:41 +0200

    Updates

---
+++ precache-manifest.d80f9d34b059a82bade1a0c0712106b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "add8a2529450e8824301",
+    "url": "/nl-trip-advisor-react/static/js/runtime~main.09a7ef84.js"
+  },
+  {
+    "revision": "ccc0e405ed4070e7f682",
+    "url": "/nl-trip-advisor-react/static/js/main.31bba37a.chunk.js"
+  },
+  {
+    "revision": "f51d143bbccedb5ea87d",
+    "url": "/nl-trip-advisor-react/static/js/2.da21de22.chunk.js"
+  },
+  {
+    "revision": "ccc0e405ed4070e7f682",
+    "url": "/nl-trip-advisor-react/static/css/main.d22db49c.chunk.css"
+  },
+  {
+    "revision": "205dba584fec88f01c1d2a5cedd23a55",
+    "url": "/nl-trip-advisor-react/index.html"
+  }
+];

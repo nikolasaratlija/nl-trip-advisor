49ded7f2ff3950147e8795b3cae769669dca8e31
Nikola Saratlija <nikola@brandsync.nl>
2019-04-30 19:57:17 +0200

    Updates

---
+++ precache-manifest.3f216717da7bfa166307471fb249a6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37d6b0928ef824f22485deb825105be5",
+    "url": "/nl-trip-advisor-react/static/media/amsterdam-architecture-building-208733.37d6b092.jpg"
+  },
+  {
+    "revision": "add8a2529450e8824301",
+    "url": "/nl-trip-advisor-react/static/js/runtime~main.09a7ef84.js"
+  },
+  {
+    "revision": "45599a214f5bd833effb",
+    "url": "/nl-trip-advisor-react/static/js/main.2a43dfc8.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/nl-trip-advisor-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "45599a214f5bd833effb",
+    "url": "/nl-trip-advisor-react/static/css/main.75ee498a.chunk.css"
+  },
+  {
+    "revision": "8ff7e404a400cf6e90d3254c9ceee5f8",
+    "url": "/nl-trip-advisor-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nl-trip-advisor-react/precache-manifest.7e1d8a0d860405ad2933f9911874c338.js"
+  "/nl-trip-advisor-react/precache-manifest.3f216717da7bfa166307471fb249a6fc.js"
 );
 
 workbox.clientsClaim();

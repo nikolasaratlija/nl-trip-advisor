b5447ce8456cc3ab8d7671bbf239972c63f74a73
Nikola Saratlija <nikola@brandsync.nl>
2019-05-01 01:29:12 +0200

    Updates

--- precache-manifest.f86a437f0339170ed4e76c71db18e92f.js
+++ precache-manifest.f86a437f0339170ed4e76c71db18e92f.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/nl-trip-advisor-react/static/js/runtime~main.09a7ef84.js"
   },
   {
-    "revision": "45599a214f5bd833effb",
-    "url": "/nl-trip-advisor-react/static/js/main.2a43dfc8.chunk.js"
+    "revision": "b6bc204e37f56233b909",
+    "url": "/nl-trip-advisor-react/static/js/main.7e542cbd.chunk.js"
   },
   {
     "revision": "681b51057d74cfa8975d",
     "url": "/nl-trip-advisor-react/static/js/2.65aa1cca.chunk.js"
   },
   {
-    "revision": "45599a214f5bd833effb",
-    "url": "/nl-trip-advisor-react/static/css/main.75ee498a.chunk.css"
+    "revision": "b6bc204e37f56233b909",
+    "url": "/nl-trip-advisor-react/static/css/main.245d878b.chunk.css"
   },
   {
-    "revision": "8ff7e404a400cf6e90d3254c9ceee5f8",
+    "revision": "6eb3b0360aecad40fa6df46678c79798",
     "url": "/nl-trip-advisor-react/index.html"
   }
 ];

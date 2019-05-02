4035208a00bcade476ebd5619534854f3951d2f6
Nikola Saratlija <nikola@brandsync.nl>
2019-05-03 00:21:39 +0200

    cleanup

--- src/assets/rotterdamData.js
+++
@@ -1,8 +0,0 @@
-const rotterdamData = {
-    lat: 51.9054439,
-    lng: 4.4666374,
-    defaultZoom: 12,
-    KmlLayerLink: "https://www.dropbox.com/s/j69qh1g9wbkutg5/rotterdam.kml?dl=1",
-};
-
-export default rotterdamData;

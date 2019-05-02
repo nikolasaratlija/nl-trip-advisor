3df46d327a3964743896d2e848750e53a4b59940
Nikola Saratlija <nikola@brandsync.nl>
2019-05-02 16:33:08 +0200

    Updates

---
+++ precache-manifest.545f6cd6f2a8b121a269fde4a39665b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7f18efcc2020853780cd37de7cdfad",
+    "url": "/nl-trip-advisor-react/index.html"
+  },
+  {
+    "revision": "7c5e5ae6d41f75b033d3",
+    "url": "/nl-trip-advisor-react/static/css/main.f9e4cb2c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/nl-trip-advisor-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7c5e5ae6d41f75b033d3",
+    "url": "/nl-trip-advisor-react/static/js/main.b1c27d7c.chunk.js"
+  },
+  {
+    "revision": "add8a2529450e8824301",
+    "url": "/nl-trip-advisor-react/static/js/runtime~main.09a7ef84.js"
+  },
+  {
+    "revision": "37d6b0928ef824f22485deb825105be5",
+    "url": "/nl-trip-advisor-react/static/media/amsterdam-architecture-building-208733.37d6b092.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nl-trip-advisor-react/precache-manifest.f86a437f0339170ed4e76c71db18e92f.js"
+  "/nl-trip-advisor-react/precache-manifest.545f6cd6f2a8b121a269fde4a39665b5.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nl-trip-advisor-react/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nl-trip-advisor-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
